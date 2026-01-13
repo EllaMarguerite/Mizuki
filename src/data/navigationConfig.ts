f9932d292b1fc08a8aef604ddbd746261d0d99c9
Ella <141297015+EllaCloudCannon@users.noreply.github.com>
2026-01-13 16:13:57 +1300

    move navigation to yaml file

---
+++ src/data/navigationConfig.ts
@@ -0,0 +1,42 @@
+// Re-export navigation metadata from YAML file
+// The YAML file is the single source of truth
+import navigationMeta from "./navigationConfig.yml";
+import { LinkPreset, type NavBarLink } from "@/types/config";
+
+// Type for YAML link structure
+type YamlLink = {
+	preset?: string;
+	name?: string;
+	url?: string;
+	icon?: string;
+	external?: boolean;
+	children?: Array<{
+		name: string;
+		url: string;
+		icon?: string;
+		external?: boolean;
+	}>;
+};
+
+export type NavigationMeta = {
+	links: YamlLink[];
+};
+
+// Convert YAML links to NavBarLink format
+function convertLinks(yamlLinks: YamlLink[]): (NavBarLink | LinkPreset)[] {
+	return yamlLinks.map((link) => {
+		// If it's a preset, return the LinkPreset enum value
+		if (link.preset) {
+			return LinkPreset[link.preset as keyof typeof LinkPreset];
+		}
+		// Otherwise, return the custom link object
+		return link as NavBarLink;
+	});
+}
+
+const rawMeta = navigationMeta as NavigationMeta;
+
+export default {
+	links: convertLinks(rawMeta.links),
+};
+
